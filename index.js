0e5f1c8b277e9a93c333bdbd8f8934926ae70134
azaadd <abulkalam.kta@gmail.com>
2023-07-07 19:35:44 +0600

    added some image link are not working add new image link in json file

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.get('/chefs', (req, res) => {
     res.send(chefs);
 })
 
+app.get('/chefs/:id', (req, res) =>{
+    const id = parseInt(req.params.id);
+    const selectedChef = chefs.find(chef => chef.id === id);
+    res.send(selectedChef);
+})
+
 app.listen(port, () => {
     console.log(`chef-recipe-hunter-server-side API is running on port: ${port}`)
 })
